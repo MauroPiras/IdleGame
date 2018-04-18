4367787fc7cb356c79315d5cc8529c36bf015aab
Mauro <mauro.piras93@gmail.com>
2018-04-19 00:54:41 +0200

    first real commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+	[SerializeField] public Text TxtConteggio;
+	private int NmbConteggio;
+	// Use this for initialization
+	void Start () {
+		TxtConteggio.text = "Souls count: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			TxtConteggio.text = "Souls count: " + ++NmbConteggio;
+		}
+	}
+}
